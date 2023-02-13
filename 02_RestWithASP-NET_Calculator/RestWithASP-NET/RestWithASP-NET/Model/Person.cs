f108445ee4b4531a4dfa73e7ec40141689b7ab37
Gabuu43 <gahgonsales43@gmail.com>
2023-02-13 13:48:16 -0300

    Add person class

---
+++ 02_RestWithASP-NET_Calculator/RestWithASP-NET/RestWithASP-NET/Model/Person.cs
@@ -0,0 +1,14 @@
+﻿namespace RestWithASP_NET.Model
+{
+    public class Person
+    {
+        public long Id { get; set; }
+
+        public long FirstName { get; set; }
+
+        public long LastName { get; set; }
+        public long AddressName { get; set; }
+        public long Gender { get; set; }
+
+    }
+}
