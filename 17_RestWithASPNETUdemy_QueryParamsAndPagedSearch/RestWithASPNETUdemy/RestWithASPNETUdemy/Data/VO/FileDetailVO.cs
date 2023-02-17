3de13a19387a9ae533d28b3f47d22e2aecf9c6f1
Gabuu43 <gahgonsales43@gmail.com>
2023-02-17 11:38:34 -0300

    Lidando com arquivos

---
+++ 17_RestWithASPNETUdemy_QueryParamsAndPagedSearch/RestWithASPNETUdemy/RestWithASPNETUdemy/Data/VO/FileDetailVO.cs
@@ -0,0 +1,9 @@
+﻿namespace RestWithASPNETUdemy.Data.VO
+{
+    public class FileDetailVO
+    {
+        public string DocumentName { get; set; }
+        public string DocumentType { get; set; }
+        public string DocumentUrl { get; set; }
+    }
+}
